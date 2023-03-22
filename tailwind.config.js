52381eacb4f9acd987b4840fda80afd4c9b0447c
Milan <milan.338@outlook.com>
2023-03-22 22:02:58 +1030

    Update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /**
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-archivo)', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'accent-blue': '#1BB8D1',
+        'accent-yellow': '#FCC018',
+        'accent-red': '#E71A64',
+        // accentHighlight: '', // TODO
+        'primary-fg': 'white',
+        // bgPrimary: '' // TODO
+      },
+      borderWidth: {
+        primary: '4px',
+      },
+      boxShadow: {
+        solid: '5px 5px',
+      },
+    },
   },
   plugins: [],
 };
