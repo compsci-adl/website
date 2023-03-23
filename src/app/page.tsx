94dd3572a029431665078f0abc3cd3fe059bcef5
Milan <milan.338@outlook.com>
2023-03-23 21:47:20 +1030

    Add initial index page

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import Article from '@/components/Article';
+import Card from '@/components/Card';
+import Hero from '@/components/Hero';
+
+export default function Page() {
+    return (
+        <main className="relative z-10 flex h-auto w-[70rem] flex-col items-center">
+            {/* TODO responsive width */}
+            <Hero />
+            <section className="grid grid-cols-3 gap-12">
+                <Card className="bg-accent-blue" heading="Learn">
+                    <Article>
+                        The club has a major focus on education and learning. We run workshops to
+                        assist students with their studies and host other educational events such as
+                        talks and guest lectures.
+                    </Article>
+                </Card>
+                <Card className="bg-accent-yellow" heading="Socialise">
+                    <Article>
+                        We are primarily a social club. Events such as meet & greets, games / movies
+                        nights, BBQs, and pub crawls are just some of the social events the club
+                        runs each year.
+                    </Article>
+                </Card>
+                <Card className="bg-accent-red" heading="Code">
+                    <Article>
+                        Coding is what we do and love. Whether you&apos;re looking for some feedback
+                        on a project or have technical questions, the club is a great way to meet
+                        and learn from others.
+                    </Article>
+                </Card>
+            </section>
+        </main>
+    );
+}
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -5,10 +5,11 @@ type ArticleProps = {
     children: ReactNode;
 };
 
+// TODO remove, just use h1 tags in children for now
 export default function Article({ title, children }: ArticleProps) {
     return (
-        <article className="flex flex-row gap-2">
-            {title !== undefined && <h1 className="font-bold">{title}</h1>}
+        <article className="flex flex-row gap-2 text-2xl leading-10">
+            {title !== undefined && <h2 className="font-bold">{title}</h2>}
             {children}
         </article>
     );
