357d578f8441ed98162f8b347242542272020f2a
Milan <milan.338@outlook.com>
2023-03-23 21:46:23 +1030

    Add initial card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,16 +8,17 @@ type CardProps = {
 
 export default function Card({ className, heading, children }: CardProps) {
     return (
-        <div>
+        <div className="flex flex-col">
             {heading !== undefined && (
                 <div
-                    className={`
-                    border-primary border-b-0 border-black p-4 shadow-solid ${className ?? ''}`}
+                    className={`border-primary border-b-0 border-black p-4 shadow-solid ${
+                        className ?? ''
+                    }`}
                 >
-                    <h1 className=" text-3xl font-bold">{heading}</h1>
+                    <h1 className=" text-4xl font-bold">{heading}</h1>
                 </div>
             )}
-            <div className="border-primary border-black bg-primary-fg p-4 shadow-solid shadow-black">
+            <div className="flex-grow border-primary border-black bg-primary-fg p-4 shadow-solid shadow-black">
                 {children}
             </div>
         </div>
