0bd7aea8111773080544c5168cc6248de68b4d40
Milan <milan.338@outlook.com>
2023-03-24 11:38:43 +1030

    Improve button animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,12 +9,15 @@ type ButtonProps = {
 
 export default function Button({ href, className, children }: ButtonProps) {
     return (
-        <Link
-            className={`flex items-center justify-center border-primary border-black shadow-solid hover:translate-x-1
-            hover:translate-y-1 motion-safe:transition-transform ${className ?? ''}`}
-            href={href}
-        >
-            {children}
-        </Link>
+        <button className="relative">
+            <Link
+                className={`z-10 flex items-center justify-center border-primary border-black hover:translate-x-[4px]
+                hover:translate-y-[4px] motion-safe:transition-transform ${className ?? ''}`}
+                href={href}
+            >
+                {children}
+            </Link>
+            <div className="absolute top-0 left-0 -z-10 box-content h-full w-full translate-x-[7px] translate-y-[7px] bg-black" />
+        </button>
     );
 }
