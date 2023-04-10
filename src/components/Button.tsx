8716fce81a4d84f75ee21343db539bed3aed8409
Ray <22254748+rayokamoto@users.noreply.github.com>
2023-04-10 12:45:59 +0930

    refactor: Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,17 +7,22 @@ type ButtonProps = JSX.IntrinsicElements["button"] & JSX.IntrinsicElements["a"] 
     className?: string;
     href?: string;
     //shadowSize?: number;
-    smallShadow?: boolean; // TODO: decrease shadow for smaller screen sizes or omit it all together
+    smallShadow?: boolean;
 }
 
 export default function Button({ children, className, href, smallShadow, ...props }: ButtonProps) {
+    const topLayerClassName = "z-10 flex h-full items-center justify-center border-primary border-black hover:translate-x-[6px] hover:translate-y-[6px] motion-safe:transition-transform";
+    const bottomLayerClassName = "absolute top-0 left-0 -z-10 box-content h-full w-full translate-x-[6px] translate-y-[6px] bg-black";
+
+    const smallShadowTopLayer = "max-sm:hover:translate-x-[2px] max-sm:hover:translate-y-[2px] max-md:hover:translate-x-[4px] max-md:hover:translate-y-[4px] max-lg:hover:translate-x-[5px] max-lg:hover:translate-y-[5px]";
+    const smallShadowBottomLayer = "max-sm:translate-y-[2px] max-sm:translate-x-[2px] max-md:translate-y-[4px] max-md:translate-x-[4px] max-lg:translate-y-[5px] max-lg:translate-x-[5px]";
+
     {/* If href is provided, use anchor tag (Link) instead of button tag */ }
     if (href !== undefined) {
         return (
             <div className="relative z-10">
                 <Link
-                    className={`z-10 flex h-full items-center justify-center border-primary border-black hover:translate-x-[6px]
-                hover:translate-y-[6px] motion-safe:transition-transform ${smallShadow === true ? 'max-md:hover:translate-x-[5px] max-md:hover:translate-y-[5px]' : ''
+                    className={`${topLayerClassName} ${smallShadow === true ? smallShadowTopLayer : ''
                         } ${className ?? ''}`}
                     href={href}
                     {...props}
@@ -25,8 +30,7 @@ export default function Button({ children, className, href, smallShadow, ...prop
                     {children}
                 </Link>
                 <div
-                    className={`absolute top-0 left-0 -z-10 box-content h-full w-full translate-x-[6px] translate-y-[6px] bg-black ${smallShadow === true ? 'max-md:translate-y-[5px] max-md:translate-x-[5px]' : ''
-                        }`}
+                    className={`${bottomLayerClassName} ${smallShadow === true ? smallShadowBottomLayer : ''}`}
                 />
             </div>
         );
@@ -35,17 +39,14 @@ export default function Button({ children, className, href, smallShadow, ...prop
     return (
         <button className="relative z-10" {...props}>
             <div
-                className={`z-10 flex h-full items-center justify-center border-primary border-black hover:translate-x-[6px]
-                hover:translate-y-[6px] motion-safe:transition-transform ${smallShadow === true ? 'max-md:hover:translate-x-[5px] max-md:hover:translate-y-[5px]' : ''}
+                className={`${topLayerClassName} ${smallShadow === true ? smallShadowTopLayer : ''}
                 ${className ?? ''}`}
             >
                 {children}
             </div>
             <div
-                className={`absolute top-0 left-0 -z-10 box-content h-full w-full translate-x-[6px] translate-y-[6px] bg-black ${smallShadow === true ? 'max-md:translate-y-[5px] max-md:translate-x-[5px]' : ''
-                    }`}
+                className={`${bottomLayerClassName} ${smallShadow === true ? smallShadowBottomLayer : ''}`}
             />
         </button>
     );
-
 }
