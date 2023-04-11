d5d814953312d061bf4d89b613ece0a69b959632
Ray <22254748+rayokamoto@users.noreply.github.com>
2023-04-11 11:55:38 +0930

    style: Format code

--- src/app/about/profiles.ts
+++ src/app/about/profiles.ts
@@ -1,17 +1,17 @@
 // TODO: Replace this with a call to the CMS to fetch committee member info
 // TODO: Maybe allow committee members to have social links in their profile cards
 export const COMMITTEE_PROFILE_CARDS = [
-    { name: "Zac Goode", role: "President" },
-    { name: "Aniza Halim", role: "Vice President" },
-    { name: "Nick Hassan", role: "Treasurer" },
-    { name: "Hattie Nguyen", role: "Secretary" },
-    { name: "Bowen Sun", role: "Sponsorship Officer" },
-    { name: "Cyrus Kelly", role: "Equity Officer" },
-    { name: "Ryan Kim", role: "\"Publicity Officer\"" },
-    { name: "Pouya Aliabad", role: "Business Manager" },
-    { name: "Steven Liu", role: "Duck Lounge Liaison" },
-    { name: "Rowan Fimmano", role: "General Committee" },
-    { name: "Chloe Walsh", role: "General Committee" },
-    { name: "Milan Sorak", role: "General Committee" },
-    { name: "Timothy Choi", role: "General Committee" },
-] as const
+    { name: 'Zac Goode', role: 'President' },
+    { name: 'Aniza Halim', role: 'Vice President' },
+    { name: 'Nick Hassan', role: 'Treasurer' },
+    { name: 'Hattie Nguyen', role: 'Secretary' },
+    { name: 'Bowen Sun', role: 'Sponsorship Officer' },
+    { name: 'Cyrus Kelly', role: 'Equity Officer' },
+    { name: 'Ryan Kim', role: '"Publicity Officer"' },
+    { name: 'Pouya Aliabad', role: 'Business Manager' },
+    { name: 'Steven Liu', role: 'Duck Lounge Liaison' },
+    { name: 'Rowan Fimmano', role: 'General Committee' },
+    { name: 'Chloe Walsh', role: 'General Committee' },
+    { name: 'Milan Sorak', role: 'General Committee' },
+    { name: 'Timothy Choi', role: 'General Committee' },
+] as const;
