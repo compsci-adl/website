cb1ceae8b28da992370296497be9454fef3abe4c
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-12-12 12:24:59 +1030

    chore: Remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Computer Science Club',
@@ -19,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     rel="stylesheet"
                 />
             </head>
-            <body className={inter.className}>{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
