3e1904b0afd74767ad44891f61f1d6ea959a3871
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-12-22 16:02:50 +1030

    fix: Use correct font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,11 +20,7 @@ const config: Config = {
         'gradient-conic':
             'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
-    fontFamily: {
-      'archivo': ['Archivo', 'sans-serif'],
-      'archivo-black': ['Archivo Black', 'sans-serif'],
-    },
+    }
   },
   plugins: [],
 };
