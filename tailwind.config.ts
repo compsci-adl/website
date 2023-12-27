aa74079e91794dac43a470fd45e619613c62a9a6
phoenixpereira <phoenixpereira99@gmail.com>
2023-12-27 18:41:56 +1030

    chore: Remove unused config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type {Config} from 'tailwindcss';
 
-// import defaultTheme from 'tailwindcss/defaultTheme';
-
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
