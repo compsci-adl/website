14096ab293c2d30e728adae6caf6f54b4021c5b2
jsun969 <i@jsun.lol>
2024-01-15 19:24:33 +1030

    refactor: reuse account layout

--- src/app/(account)/layout.tsx
+++ src/app/(account)/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
-export default function JoinUsLayout({ children }: RootLayoutProps) {
+export default function AccountLayout({ children }: RootLayoutProps) {
     return (
         <>
             <Header />
