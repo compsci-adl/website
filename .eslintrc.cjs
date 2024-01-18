14b3337ecc58354a4264ad442cbc86c20bd1162d
jsun969 <i@jsun.lol>
2024-01-18 22:11:07 +1030

    chore(eslint): warn `any` instead of error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,6 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
         '@typescript-eslint/consistent-type-imports': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
     },
 };
