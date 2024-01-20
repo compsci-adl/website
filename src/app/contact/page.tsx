55abda62b6519c78cbf6617a4aa569f2e1f25733
jsun969 <i@jsun.lol>
2024-01-20 12:59:23 +1030

    fix(contact): background image filename

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ import Title from './Title';
 export default function ContactPage() {
     return (
         <div className="md:pt-40 pt-32 min-h-screen w-responsive mx-auto">
-            <div className="bg-[url('/images/rectanglegrid.svg')] md:bg-no-repeat bg-repeat-y md:bg-center md:bg-[length:90%_90%] h-full">
+            <div className="bg-[url('/images/rectangleGrid.svg')] md:bg-no-repeat bg-repeat-y md:bg-center md:bg-[length:90%_90%] h-full">
                 <div className="grid md:grid-cols-2 grid-cols-1 md:gap-10 gap-8">
                     <Title className="md:col-span-2 grid-flow-dense justify-self-center" />
                     <div className="md:space-y-20 space-y-8">
