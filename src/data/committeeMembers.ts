16d44891c37cafe01bd660fe85b49c15d684526b
phoenixpereira <phoenixpereira99@gmail.com>
2024-02-10 16:56:41 +1030

    style: run prettier

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,119 +1,119 @@
 // List of committee members with name, role and whether they are part of the
 // executive committee
 export const committeeMembers = [
-  {
-    name: 'David Maslov',
-    position: 'President',
-    isExecCommittee: true,
-  },
-  {
-    name: 'Cubie',
-    position: 'Vice-President',
-    isExecCommittee: true,
-  },
-  {
-    name: 'Christian',
-    position: 'Treasurer',
-    isExecCommittee: true,
-  },
-  {
-    name: 'Ray Okamoto',
-    position: 'Secretary',
-    isExecCommittee: true,
-  },
-  {
-    name: 'Milan',
-    position: 'Partnerships & Sponsorships Manager',
-    isExecCommittee: true,
-  },
-  {
-    name: 'F. Umar',
-    position: 'Partnerships & Sponsorships Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Pouya',
-    position: 'Partnerships & Sponsorships Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Bowen Sun',
-    position: 'Business Manager',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Willard Gorman',
-    position: 'Duck Lounge Liaison',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Mischa Sarac',
-    position: 'Equity Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'S. Bhatta',
-    position: 'Artificial Intelligence Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Will Johnston',
-    position: 'Cybersecurity Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Khanh',
-    position: 'Data Science Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Jonty Leslie',
-    position: 'Distributed Systems & Networking Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Joshua Goldsworthy Callaway',
-    position: 'Information Technology Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Dino Macri',
-    position: 'Software Engineering Representative',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Yuhan Wang',
-    position: 'Graphic Designer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Darcy',
-    position: 'Social Media Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'M. Klaric',
-    position: 'Social Media Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Phoenix Pereira',
-    position: 'Open Source Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Rishi Adhikari',
-    position: 'Open Source Officer',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Cuinn Kemp',
-    position: 'General Committee',
-    isExecCommittee: false,
-  },
-  {
-    name: 'Timothy Choi',
-    position: 'General Committee',
-    isExecCommittee: false,
-  },
+    {
+        name: 'David Maslov',
+        position: 'President',
+        isExecCommittee: true,
+    },
+    {
+        name: 'Cubie',
+        position: 'Vice-President',
+        isExecCommittee: true,
+    },
+    {
+        name: 'Christian',
+        position: 'Treasurer',
+        isExecCommittee: true,
+    },
+    {
+        name: 'Ray Okamoto',
+        position: 'Secretary',
+        isExecCommittee: true,
+    },
+    {
+        name: 'Milan',
+        position: 'Partnerships & Sponsorships Manager',
+        isExecCommittee: true,
+    },
+    {
+        name: 'F. Umar',
+        position: 'Partnerships & Sponsorships Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Pouya',
+        position: 'Partnerships & Sponsorships Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Bowen Sun',
+        position: 'Business Manager',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Willard Gorman',
+        position: 'Duck Lounge Liaison',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Mischa Sarac',
+        position: 'Equity Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'S. Bhatta',
+        position: 'Artificial Intelligence Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Will Johnston',
+        position: 'Cybersecurity Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Khanh',
+        position: 'Data Science Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Jonty Leslie',
+        position: 'Distributed Systems & Networking Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Joshua Goldsworthy Callaway',
+        position: 'Information Technology Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Dino Macri',
+        position: 'Software Engineering Representative',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Yuhan Wang',
+        position: 'Graphic Designer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Darcy',
+        position: 'Social Media Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'M. Klaric',
+        position: 'Social Media Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Phoenix Pereira',
+        position: 'Open Source Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Rishi Adhikari',
+        position: 'Open Source Officer',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Cuinn Kemp',
+        position: 'General Committee',
+        isExecCommittee: false,
+    },
+    {
+        name: 'Timothy Choi',
+        position: 'General Committee',
+        isExecCommittee: false,
+    },
 ] as const;
