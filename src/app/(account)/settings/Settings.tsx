42aba0918101f35173130ce4b364707ff09b27d8
jsun969 <i@jsun.lol>
2024-02-17 02:19:23 +1030

    fix(settings): tab titles

--- src/app/(account)/settings/Settings.tsx
+++ src/app/(account)/settings/Settings.tsx
@@ -4,18 +4,18 @@ import { useState } from 'react';
 import Sidebar from './Sidebar';
 import MembershipSettings from './tabs/MembershipSettings';
 
-export const TAB_NAMES = ['account', 'personalInfo', 'membership', 'notifications'] as const;
+export const TAB_NAMES = ['Account', 'Personal Info', 'Membership', 'Notifications'] as const;
 export type TabNames = (typeof TAB_NAMES)[number];
 
 const SETTING_TABS = {
-    account: <></>,
-    personalInfo: <></>,
-    membership: <MembershipSettings />,
-    notifications: <></>,
+    Account: <></>,
+    'Personal Info': <></>,
+    Membership: <MembershipSettings />,
+    Notifications: <></>,
 } as const satisfies Record<TabNames, React.ReactNode>;
 
 export default function Settings() {
-    const [tab, setTab] = useState<TabNames>('membership');
+    const [tab, setTab] = useState<TabNames>('Membership');
 
     return (
         <>
