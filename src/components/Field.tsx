53e411da33d955ac27ce294391214f98bb3053e2
jsun969 <i@jsun.lol>
2024-02-16 11:25:29 +1030

    fix(field/select): remove unnecessary `selected`

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -47,7 +47,7 @@ const Field = ({
                 >
                     {placeholder && <option value="">{placeholder}</option>}
                     {options.map((option, index) => (
-                        <option key={index} value={option} selected={option === defaultValue}>
+                        <option key={index} value={option}>
                             {option}
                         </option>
                     ))}
