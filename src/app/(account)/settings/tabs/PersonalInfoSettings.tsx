4b0a3d0b36a7f9021b5f724b1eb5b2c504f7acc3
jsun969 <i@jsun.lol>
2024-02-17 00:11:54 +1030

    refactor(settings): move setting tabs to `tabs` folder

--- src/app/(account)/settings/tabs/PersonalInfoSettings.tsx
+++ src/app/(account)/settings/tabs/PersonalInfoSettings.tsx
@@ -1,6 +1,6 @@
 import Button from '@/components/Button';
 import ControlledField from '@/components/ControlledField';
-import { AGE_BRACKETS, GENDERS, STUDENT_TYPES, STUDENT_STATUSES } from '@/constants/student-info';
+import { AGE_BRACKETS, GENDERS, STUDENT_STATUSES, STUDENT_TYPES } from '@/constants/student-info';
 import { useMount } from '@/hooks/use-mount';
 import { useUser } from '@clerk/nextjs';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -66,27 +66,14 @@ export default function PersonalInfoSettings() {
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
             <h2 className="text-2xl font-bold">Change Personal Info</h2>
             <div className="mb-2 border-b-2 border-black"></div>
-            <ControlledField
-                label="First Name"
-                name="firstName"
-                control={control}
-                type="text"
-                defaultValue={formData.firstName}
-            />
-            <ControlledField
-                label="Last Name"
-                name="lastName"
-                control={control}
-                type="text"
-                defaultValue={formData.lastName}
-            />
+            <ControlledField label="First Name" name="firstName" control={control} type="text" />
+            <ControlledField label="Last Name" name="lastName" control={control} type="text" />
             <ControlledField
                 label="Age"
                 name="ageBracket"
                 control={control}
                 type="select"
                 options={AGE_BRACKETS}
-                defaultValue={formData.ageBracket}
             />
             <ControlledField
                 label="Gender"
@@ -94,7 +81,6 @@ export default function PersonalInfoSettings() {
                 control={control}
                 type="select"
                 options={GENDERS}
-                defaultValue={formData.gender}
             />
             <ControlledField
                 label="Student Type"
@@ -102,7 +88,6 @@ export default function PersonalInfoSettings() {
                 control={control}
                 type="select"
                 options={STUDENT_TYPES}
-                defaultValue={formData.studentType}
             />
             <ControlledField
                 label="Student Status"
@@ -110,15 +95,8 @@ export default function PersonalInfoSettings() {
                 control={control}
                 type="select"
                 options={STUDENT_STATUSES}
-                defaultValue={formData.studentStatus}
             />
-            <ControlledField
-                label="Student ID"
-                name="studentId"
-                control={control}
-                type="text"
-                defaultValue={formData.studentId}
-            />
+            <ControlledField label="Student ID" name="studentId" control={control} type="text" />
             <Button type="submit" colour="orange">
                 Update personal info
             </Button>
