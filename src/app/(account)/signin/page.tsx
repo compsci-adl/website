a5d393d2381b24abcb296151aea29b5c31c721fc
Yeyang (Justin) Sun <i@jsun.lol>
2024-02-19 04:03:05 +1030

    fix(signin): header buttons doesn't change after signin (#75)

--- src/app/(account)/signin/page.tsx
+++ src/app/(account)/signin/page.tsx
@@ -6,7 +6,6 @@ import FancyRectangle from '@/components/FancyRectangle';
 import { useSignIn } from '@clerk/clerk-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { FcGoogle } from 'react-icons/fc';
 import { z } from 'zod';
@@ -26,8 +25,6 @@ export default function SignInPage() {
         resolver: zodResolver(signInSchema),
     });
 
-    const router = useRouter();
-
     const handleSignIn = form.handleSubmit(async ({ email, password }) => {
         if (!isLoaded) return;
         try {
@@ -38,7 +35,7 @@ export default function SignInPage() {
 
             if (result.status === 'complete') {
                 await setActive({ session: result.createdSessionId });
-                router.push('/');
+                location.href = '/';
             } else {
                 console.log(result);
             }
