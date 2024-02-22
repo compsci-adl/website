1d1a652c648a6fea5447cfb19e11b6704578a691
jsun969 <i@jsun.lol>
2024-02-23 04:00:48 +1030

    fix(settings): cache redundant query in settings and header

--- src/server/check-user-exists.ts
+++ src/server/check-user-exists.ts
@@ -1,11 +1,12 @@
 import { db } from '@/db';
 import { memberTable } from '@/db/schema';
 import { eq } from 'drizzle-orm';
+import { cache } from 'react';
 
-export const checkUserExists = async (clerkUserId: string) => {
+export const checkUserExists = cache(async (clerkUserId: string) => {
     const existingUser = await db
         .select({ count: memberTable.id })
         .from(memberTable)
         .where(eq(memberTable.clerkId, clerkUserId));
     return existingUser.length > 0;
-};
+});
