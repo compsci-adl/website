a6ce1155f8e199aa94a97ef2178ac9b1ac1f978e
jsun969 <i@jsun.lol>
2024-02-24 20:59:33 +1030

    fix: add clerk fallback url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,16 @@
 import { authMiddleware } from '@clerk/nextjs';
 
-const authRoutes = ['/account', '/dashboard', '/settings', '/admin'];
-
 export default authMiddleware({
-    publicRoutes: (req) => {
-        return !authRoutes.includes(req.nextUrl.pathname);
-    },
+    publicRoutes: [
+        '/',
+        '/about',
+        '/contact',
+        '/events',
+        '/sponsors',
+        '/signin',
+        '/join',
+        '/forgot-password',
+    ],
 });
 
 export const config = {
