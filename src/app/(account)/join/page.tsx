518ad4d473962c385d07e7fbc27e2ffa051659ba
jsun969 <i@jsun.lol>
2024-02-24 21:11:21 +1030

    fix: check user before `signin` / `join`

--- src/app/(account)/join/page.tsx
+++ src/app/(account)/join/page.tsx
@@ -1,10 +1,20 @@
+import { checkUserExists } from '@/server/check-user-exists';
+import { currentUser } from '@clerk/nextjs';
 import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import Join from './Join';
 
 export const metadata: Metadata = {
     title: 'Join',
 };
 
-export default function JoinPage() {
+export default async function JoinPage() {
+    const user = await currentUser();
+    if (user) {
+        const userExists = await checkUserExists(user.id);
+        if (userExists) {
+            redirect('/settings');
+        }
+    }
     return <Join />;
 }
--- src/app/(account)/signin/page.tsx
+++ src/app/(account)/signin/page.tsx
@@ -1,10 +1,14 @@
+import { currentUser } from '@clerk/nextjs';
 import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import SignIn from './SignIn';
 
 export const metadata: Metadata = {
     title: 'Sign In',
 };
 
-export default function SignInPage() {
+export default async function SignInPage() {
+    const user = await currentUser();
+    if (user) redirect('/settings');
     return <SignIn />;
 }
