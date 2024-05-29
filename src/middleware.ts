2420784717ad8f49660ea397f5db751b7cfbddfa
Ray <22254748+rayokamoto@users.noreply.github.com>
2024-05-29 12:53:24 +0930

    chore: remove comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,3 @@
-// import { authMiddleware } from '@clerk/nextjs';
-// const authRoutes = ['/settings', '/admin'];
-// export default authMiddleware({
-//     publicRoutes: (req) => !authRoutes.includes(req.url),
-// });
-// export const config = {
-//     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher(['/settings(.*)', '/admin(.*)']);
