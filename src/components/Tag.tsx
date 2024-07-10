5622d20064654e41be8562560739d8ae2591c1c1
Terry Cheng <cyr062599@gmail.com>
2024-07-10 14:18:40 +0930

    feat: Add function to calculates backgroundColor, returns black or white as the textColor for Tag component

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,13 +1,17 @@
+import { getContrastColor } from '@/constants/colours';
+
 interface TagProps {
     name: string;
     backgroundColor: string;
 }
 
 export default function Tag({ name, backgroundColor }: TagProps) {
+    const textColor: string = getContrastColor(backgroundColor);
+
     return (
         <div
-            className={`h-fit w-fit rounded-xl border-2 border-black px-3 py-1.5 text-white`}
-            style={{ backgroundColor }}
+            className={`h-fit w-fit rounded-xl border-2 border-black px-3 py-1.5`}
+            style={{ backgroundColor, color: textColor }}
         >
             <span>{name}</span>
         </div>
--- src/constants/colours.ts
+++ src/constants/colours.ts
@@ -30,3 +30,19 @@ export const TECH_COLORS: { [key: string]: string } = {
     'Tailwind CSS': '#907FC3',
     TypeScript: '#ACCB00',
 };
+
+/**
+ * Calculates the perceived luminance and returns black or white as the contrast color. Based on
+ * code from matfin. Source: https://stackoverflow.com/a/44615197/24033621, licensed under CC BY-SA
+ * 3.0.
+ */
+
+export const getContrastColor = function (backgroundColor: string) {
+    const hex = backgroundColor.replace(/^#/, '');
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    const textColor = luminance > 0.5 ? '#000000' : '#FFFFFF';
+    return textColor;
+};
