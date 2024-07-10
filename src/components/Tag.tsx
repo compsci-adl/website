9166e27cb493d3632c76b93ef5d6ce09b8c37a83
Terry Cheng <cyr062599@gmail.com>
2024-07-10 20:06:32 +0930

    chore: Update Tag component code styling

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -10,7 +10,7 @@ export default function Tag({ name, backgroundColor }: TagProps) {
 
     return (
         <div
-            className={`h-fit w-fit rounded-xl border-2 border-black px-3 py-1.5`}
+            className="h-fit w-fit rounded-xl border-2 border-black px-3 py-1.5"
             style={{ backgroundColor, color: textColor }}
         >
             <span>{name}</span>
