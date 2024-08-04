9b1d7e7599a9f5d0c6edf32e88f6c90b47ced543
Mahir Islam <mahir.islam.numayeer@gmail.com>
2024-08-04 22:31:51 +0930

    should work

--- src/data/future-projects.ts
+++ src/data/future-projects.ts
@@ -10,20 +10,27 @@ export const FUTURE_PROJECTS: FutureProject[] = [
     {
         title: "Courses API",
         description:
-            'Scrapes course info from the UofA website and provides course data for other projects through an API endpoint.',
-        techStacks: ['Python', 'FastAPI', 'Requests'],
+            "Scrapes course info from the UofA website and provides course data for other projects through an API endpoint.",
+        techStacks: ["Python", "FastAPI", "Requests"],
     },
     {
         title: "MyTimetable",
         description:
-            'An interactive drag-and-drop timetable scheduler to help UofA students optimise their weekly timetable.',
-        techStacks: ['TypeScript', 'React', 'Vite', 'Pragmatic Drag & Drop', 'NextUI', 'Tailwind CSS'],
+            "An interactive drag-and-drop timetable scheduler to help UofA students optimise their weekly timetable.",
+        techStacks: [
+            "TypeScript",
+            "React",
+            "Vite",
+            "Pragmatic Drag & Drop",
+            "NextUI",
+            "Tailwind CSS",
+        ],
     },
     {
         title: "MyStudyPlan",
         description:
-            'A UofA degree planner that allows you to explore and validate your degree structure.',
-        techStacks: ['TypeScript', 'React', 'Vite', 'Tailwind CSS'],
+            "A UofA degree planner that allows you to explore and validate your degree structure.",
+        techStacks: ["TypeScript", "React", "Vite", "Tailwind CSS"],
     },
     {
         title: "MyCourseReviews",
