f1c6a8d02d1c6fba20eac3991c74e8686ceba150
phoenixpereira <phoenixpereira99@gmail.com>
2025-01-03 13:50:57 +1030

    feat: Add welcome email

--- emails/socialIcon.tsx
+++ emails/socialIcon.tsx
@@ -12,37 +12,36 @@ function SocialIcon({ href, alt, lightSrc, darkSrc, ariaLabel }: SocialIconProps
     return (
         <td className="p-2">
             <table className="border-spacing-0">
-                <tr className="h-[0.3935rem] w-[0.3rem]">
-                    <td className="border-trueWhite border-0 border-l-[3px] border-t-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                    <td className="border-trueWhite border-0 border-r-[3px] border-t-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                    <td className="h-[0.3rem] w-[0.3rem]"></td>
+                <tr className="h-[0.3255rem] w-[0.2rem]">
+                    <td className="dynamicBorder border-0 border-l-[3px] border-t-[3px] border-solid bg-white dark:bg-grey"></td>
+                    <td className="dynamicBorder border-0 border-r-[3px] border-t-[3px] border-solid bg-white dark:bg-grey"></td>
+                    <td className="h-[0.3rem] w-[0.2rem]"></td>
                 </tr>
                 <tr>
-                    <td className="border-trueWhite h-[0.3rem] w-[0.3rem] border-0 border-b-[3px] border-l-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                    <td className="border-trueWhite border-0 border-b-[3px] border-r-[3px] border-solid bg-grey p-0 dark:border-black dark:bg-white">
+                    <td className="dynamicBorder h-[0.3rem] w-[0.1125rem] border-0 border-b-[3px] border-l-[3px] border-solid bg-white dark:bg-grey"></td>
+                    <td className="dynamicBorder border-0 border-b-[3px] border-r-[3px] border-solid bg-white p-0 dark:bg-grey">
                         <div className="mx-0 my-auto h-[2.25rem] w-[2.25rem]">
                             <a
                                 href={href}
                                 aria-label={ariaLabel}
-                                className="box-border inline-block h-[2.25rem] w-[2.25rem] p-1"
+                                className="ml-[0.09rem] box-border inline-block h-[2.25rem] w-[2.25rem] p-1"
                             >
-                                <picture>
-                                    <source
-                                        srcSet={lightSrc}
-                                        media="(prefers-color-scheme: light) "
-                                    />
-                                    <Img
-                                        className="mr-[0.3rem] mt-[0.05rem] h-[1.35rem] w-[1.35rem] object-contain"
-                                        src={darkSrc}
-                                        alt={alt}
-                                    />
-                                </picture>
+                                <Img
+                                    className="light mr-[0.4rem] mt-[0.05rem] h-[1.35rem] w-[1.35rem] object-contain"
+                                    src={darkSrc}
+                                    alt={alt}
+                                />
+                                <Img
+                                    className="dark mr-[0.4rem] mt-[0.05rem] h-[1.35rem] w-[1.35rem] object-contain"
+                                    src={lightSrc}
+                                    alt={alt}
+                                />
                             </a>
                         </div>
                     </td>
-                    <td className="bg-bg h-[0.3rem] w-[0.3rem]"></td>
+                    <td className="bg-bg h-[0.3rem] w-[0.2rem]"></td>
                 </tr>
-                <tr className="h-[0.3935rem] w-[0.3rem]">
+                <tr className="h-[0.3255rem] w-[0.1125rem]">
                     <td></td>
                     <td className="bg-bg"></td>
                     <td className="bg-bg"></td>
--- emails/welcome.tsx
+++ emails/welcome.tsx
@@ -1,5 +1,16 @@
-import { Head, Img, Body, Container, Tailwind } from '@react-email/components';
+import {
+    Head,
+    Html,
+    Img,
+    Body,
+    Container,
+    Tailwind,
+    Section,
+    Row,
+    Column,
+} from '@react-email/components';
 import * as React from 'react';
+import EmailBody from './body';
 import { socialLinks } from './links';
 import SocialIcon from './socialIcon';
 
@@ -29,98 +40,130 @@ export default function Email() {
                 },
             }}
         >
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="color-scheme" content="light dark" />
-                <meta name="supported-color-schemes" content="light dark" />
-                <title>CS Club</title>
-            </Head>
-            <Body className="m-0 mt-8 bg-white p-0 font-sans text-black dark:bg-grey">
-                <Container className="bg-white dark:bg-grey">
-                    {/* Header */}
-                    <div className="bg-white p-2.5 text-center dark:bg-grey">
-                        <table className="w-full border-collapse">
-                            <tr>
-                                <td className="text-center">
-                                    <picture>
-                                        <source
-                                            srcSet="https://csclub.org.au/images/email/dark/logo.png"
-                                            media="(prefers-color-scheme: dark)"
-                                        />
+            <Html>
+                <Head>
+                    <style>
+                        {`
+  /* Standard root color scheme declaration */
+  :root {
+    color-scheme: light dark;
+  }
+
+  /* Media query for dark mode */
+  .light {
+      display: inline !important; /* Ensure it's hidden in dark mode */
+    }
+    .dark {
+      display: none !important; /* Ensure dark logo is shown in dark mode */
+    }
+      .dynamicBorder{
+      border-color: #252020;
+      }
+
+  @media (prefers-color-scheme: dark) {
+    .light {
+      display: none !important; /* Ensure it's hidden in dark mode */
+    }
+    .dark {
+      display: inline !important; /* Ensure dark logo is shown in dark mode */
+    }
+      .dynamicBorder{
+      border-color: #F3F3EB;
+      }
+  }
+    `}
+                    </style>
+                    <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="color-scheme" content="light dark" />
+                    <meta name="supported-color-schemes" content="light dark" />
+                    <title>CS Club</title>
+                </Head>
+                <Body className="m-0 mt-8 bg-white p-0 font-sans text-black dark:bg-grey">
+                    <Container className="bg-white dark:bg-grey">
+                        {/* Header */}
+                        <Container className="bg-white p-2.5 text-center dark:bg-grey">
+                            <Section className="w-full border-collapse">
+                                <Row>
+                                    <Column className="text-center">
                                         <Img
-                                            className="inline-block max-h-[60px]"
+                                            className="light max-h-[60px]"
                                             src="https://csclub.org.au/images/email/light/logo.png"
                                             alt="CS Club"
                                         />
-                                    </picture>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-
-                    {/* Email Content */}
-                    <div className="mx-10 my-2">
-                        <table className="w-full border-spacing-0">
-                            <tr className="h-[0.3935rem] w-2">
-                                <td className="border-trueWhite border-0 border-l-[3px] border-t-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                                <td className="border-trueWhite border-0 border-r-[3px] border-t-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                                <td className="h-[0.3rem] w-[0.3rem]"></td>
-                            </tr>
-                            <tr>
-                                <td className="border-trueWhite h-[0.3rem] w-[0.3rem] border-0 border-b-[3px] border-l-[3px] border-solid bg-grey dark:border-black dark:bg-white"></td>
-                                <td className="border-trueWhite border-0 border-b-[3px] border-r-[3px] border-solid bg-grey p-5 dark:border-black dark:bg-white">
-                                    <p className="text-white dark:text-black">Test</p>
-                                </td>
-                                <td className="h-[0.3rem] w-[0.3rem] bg-purple"></td>
-                            </tr>
-                            <tr className="h-[0.3935rem] w-2">
-                                <td></td>
-                                <td className="bg-purple"></td>
-                                <td className="bg-purple"></td>
-                            </tr>
-                        </table>
-                    </div>
-
-                    {/* Footer */}
-                    <div className="bg-white px-5 text-center dark:bg-grey">
-                        <div>
-                            <table className="mx-auto border-collapse border-spacing-0">
-                                <tr>
-                                    {socialLinks.slice(0, 4).map((link, index) => (
-                                        <SocialIcon
-                                            key={index}
-                                            href={link.href}
-                                            alt={link.alt}
-                                            lightSrc={link.lightSrc}
-                                            darkSrc={link.darkSrc}
-                                            ariaLabel={link.ariaLabel}
+                                        <Img
+                                            className="dark max-h-[60px]"
+                                            src="https://csclub.org.au/images/email/dark/logo.png"
+                                            alt="CS Club"
                                         />
-                                    ))}
+                                    </Column>
+                                </Row>
+                            </Section>
+                        </Container>
+
+                        {/* Email Content */}
+                        <div className="mx-6 mb-8 mt-4 md:mx-10">
+                            <table className="w-full border-spacing-0">
+                                <tr className="h-[0.3935rem] w-2">
+                                    <td className="dynamicBorder border-0 border-l-[3px] border-t-[3px] border-solid bg-white dark:bg-grey"></td>
+                                    <td className="dynamicBorder border-0 border-r-[3px] border-t-[3px] border-solid bg-white dark:bg-grey"></td>
+                                    <td className="h-[0.3rem] w-[0.3rem]"></td>
                                 </tr>
-
-                                <tr className="h-4"></tr>
-
                                 <tr>
-                                    {socialLinks.slice(4).map((link, index) => (
-                                        <SocialIcon
-                                            key={index}
-                                            href={link.href}
-                                            alt={link.alt}
-                                            lightSrc={link.lightSrc}
-                                            darkSrc={link.darkSrc}
-                                            ariaLabel={link.ariaLabel}
-                                        />
-                                    ))}
+                                    <td className="dynamicBorder h-[0.3rem] w-[0.3rem] border-0 border-b-[3px] border-l-[3px] border-solid bg-white dark:bg-grey"></td>
+                                    <td className="dynamicBorder border-0 border-b-[3px] border-r-[3px] border-solid bg-white p-5 dark:bg-grey">
+                                        <EmailBody />
+                                    </td>
+                                    <td className="h-[0.3rem] w-[0.3rem] bg-purple"></td>
+                                </tr>
+                                <tr className="h-[0.3935rem] w-2">
+                                    <td></td>
+                                    <td className="bg-purple"></td>
+                                    <td className="bg-purple"></td>
                                 </tr>
                             </table>
                         </div>
-                        <p className="text-center text-sm text-black dark:text-white">
-                            © {currentYear} The University of Adelaide Computer Science Club.
-                        </p>
-                    </div>
-                </Container>
-            </Body>
+
+                        {/* Footer */}
+                        <div className="mb-12 bg-white px-5 text-center dark:bg-grey">
+                            <div>
+                                <table className="mx-auto border-collapse border-spacing-0">
+                                    <tr>
+                                        {socialLinks.slice(0, 4).map((link, index) => (
+                                            <SocialIcon
+                                                key={index}
+                                                href={link.href}
+                                                alt={link.alt}
+                                                lightSrc={link.lightSrc}
+                                                darkSrc={link.darkSrc}
+                                                ariaLabel={link.ariaLabel}
+                                            />
+                                        ))}
+                                    </tr>
+
+                                    <tr className="h-4"></tr>
+
+                                    <tr>
+                                        {socialLinks.slice(4).map((link, index) => (
+                                            <SocialIcon
+                                                key={index}
+                                                href={link.href}
+                                                alt={link.alt}
+                                                lightSrc={link.lightSrc}
+                                                darkSrc={link.darkSrc}
+                                                ariaLabel={link.ariaLabel}
+                                            />
+                                        ))}
+                                    </tr>
+                                </table>
+                            </div>
+                            <p className="text-center text-sm text-black dark:text-white">
+                                © {currentYear} The University of Adelaide Computer Science Club.
+                            </p>
+                        </div>
+                    </Container>
+                </Body>
+            </Html>
         </Tailwind>
     );
 }
