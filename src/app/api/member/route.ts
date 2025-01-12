b6025fdacefd1c04390d2f9ed0f46a1a521dfc14
phoenixpereira <phoenixpereira99@gmail.com>
2025-01-12 12:10:10 +1030

    chore(api): Remove else statement and remove console logs

--- src/app/api/member/route.ts
+++ src/app/api/member/route.ts
@@ -15,14 +15,12 @@ export async function POST(request: Request) {
 
         const session = await auth();
         if (!session?.user) {
-            console.warn('Unauthenticated request');
             return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
         }
 
         const reqBody = schema.safeParse(req);
 
         if (!reqBody.success) {
-            console.error('Invalid request body:', reqBody.error.format());
             return new Response(JSON.stringify(reqBody.error.format()), { status: 400 });
         }
 
@@ -41,23 +39,22 @@ export async function POST(request: Request) {
                 JSON.stringify({ error: 'Member Keycloak ID missing or no notifications data' }),
                 { status: 400 }
             );
-        } else {
-            await db.insert(notificationsTable).values({
-                keycloakId: session.user.id,
-                emailNewsletters: notificationsData.email?.newsletters ?? false,
-                emailClubEventsAndAnnouncements:
-                    notificationsData.email?.clubEventsAndAnnouncements ?? false,
-                emailSponsorNotifications: notificationsData.email?.sponsorNotifications ?? false,
-                smsNewsletters: notificationsData.sms?.newsletters ?? false,
-                smsClubEventsAndAnnouncements:
-                    notificationsData.sms?.clubEventsAndAnnouncements ?? false,
-                smsSponsorNotifications: notificationsData.sms?.sponsorNotifications ?? false,
-            });
         }
 
+        await db.insert(notificationsTable).values({
+            keycloakId: session.user.id,
+            emailNewsletters: notificationsData.email?.newsletters ?? false,
+            emailClubEventsAndAnnouncements:
+                notificationsData.email?.clubEventsAndAnnouncements ?? false,
+            emailSponsorNotifications: notificationsData.email?.sponsorNotifications ?? false,
+            smsNewsletters: notificationsData.sms?.newsletters ?? false,
+            smsClubEventsAndAnnouncements:
+                notificationsData.sms?.clubEventsAndAnnouncements ?? false,
+            smsSponsorNotifications: notificationsData.sms?.sponsorNotifications ?? false,
+        });
+
         return new Response(JSON.stringify({ success: true }), { status: 200 });
-    } catch (error) {
-        console.error('Error in POST /api/member:', error);
+    } catch {
         return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
     }
 }
