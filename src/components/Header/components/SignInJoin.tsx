e024c15273b7e570bd79dbd2aa647c40f4934f56
phoenixpereira <phoenixpereira99@gmail.com>
2025-02-05 19:24:16 +1030

    fix(auth): Implement trusted origin handling for authentication requests

--- src/components/Header/components/SignInJoin.tsx
+++ src/components/Header/components/SignInJoin.tsx
@@ -1,14 +1,11 @@
-import { env } from '@/env.mjs';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import Button from '../../Button';
 
 export function SignInJoin() {
-    const redirectUri = `${env.NEXT_PUBLIC_KEYCLOAK_REDIRECT_URI}?registered`;
-    const authUrl = `${env.NEXT_PUBLIC_AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/auth?response_type=code&client_id=website&redirect_uri=${redirectUri}&scope=openid+profile+email`;
     return (
         <>
-            <Button colour="orange" href={authUrl}>
+            <Button colour="orange" onClick={() => signIn('keycloak')}>
                 Sign In
             </Button>
             <Button colour="purple" href="/join">
