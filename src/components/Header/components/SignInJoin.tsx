9c1824449a82f34f1c81c457b3e7b45db4fa90ab
phoenixpereira <phoenixpereira99@gmail.com>
2025-02-05 15:05:38 +1030

    fix(auth): Update sign-in button to use dynamic authentication URL

--- src/components/Header/components/SignInJoin.tsx
+++ src/components/Header/components/SignInJoin.tsx
@@ -1,11 +1,14 @@
+import { env } from '@/env.mjs';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import Button from '../../Button';
 
 export function SignInJoin() {
+    const redirectUri = `${env.NEXT_PUBLIC_KEYCLOAK_REDIRECT_URI}?registered`;
+    const authUrl = `${env.NEXT_PUBLIC_AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/auth?response_type=code&client_id=website&redirect_uri=${redirectUri}&scope=openid+profile+email`;
     return (
         <>
-            <Button colour="orange" onClick={() => signIn('keycloak')}>
+            <Button colour="orange" href={authUrl}>
                 Sign In
             </Button>
             <Button colour="purple" href="/join">
