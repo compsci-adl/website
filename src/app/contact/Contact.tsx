c9948036554cbe414809f911cc485013112190a6
Phoenix Pereira <47909638+phoenixpereira@users.noreply.github.com>
2025-03-17 10:28:29 +1030

    chore: Remove contact us form (#260)
    * chore: Remove contact us form
    
    * chore: Remove contact email route and related environment variable

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -31,7 +31,7 @@ export default function Contact({ className }: { className?: string }) {
                 <br />
                 To keep up to date with our news and events, check out our Instagram!
             </Paragraph>
-            <div className="flex flex-wrap justify-start gap-4 text-3xl md:text-5xl">
+            <div className="flex flex-wrap justify-center gap-4 text-3xl md:text-5xl">
                 {LINKS.map((link, i) => (
                     <Link {...link} borderColour={LINK_BORDER_COLOURS[link.name]} key={i} />
                 ))}
