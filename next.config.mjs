ae41835537d3b3cec704f9f1f77905a322c4346c
phoenixpereira <phoenixpereira99@gmail.com>
2025-05-07 17:30:39 +0930

    fix: Safeguard against undefined NEXT_PUBLIC_PAYLOAD_URI

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,12 @@ const nextConfig = {
                 hostname: 'www.gravatar.com',
             },
             {
-                protocol: process.env.NEXT_PUBLIC_PAYLOAD_URI.startsWith('https')
+                protocol: process.env.NEXT_PUBLIC_PAYLOAD_URI?.startsWith('https')
                     ? 'https'
                     : 'http',
-                hostname: new URL(process.env.NEXT_PUBLIC_PAYLOAD_URI).hostname,
+                hostname: process.env.NEXT_PUBLIC_PAYLOAD_URI
+                    ? new URL(process.env.NEXT_PUBLIC_PAYLOAD_URI).hostname
+                    : 'localhost',
                 port: new URL(process.env.NEXT_PUBLIC_PAYLOAD_URI).port || '',
                 pathname: '/api/media/file/**',
             },
