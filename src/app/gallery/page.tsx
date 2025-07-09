23756ccc9ae04474c88cfec283014597a84e8447
phoenixpereira <phoenixpereira99@gmail.com>
2025-07-09 21:24:55 +0930

    feat: Add empty photo gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,64 @@
+import { auth } from '@/auth';
+import FancyRectangle from '@/components/FancyRectangle';
+import Title from '@/components/Title';
+import { checkUserExists } from '@/server/check-user-exists';
+import { sendWelcomeEmail } from '@/server/send-welcome-email';
+import { verifyMembershipPayment } from '@/server/verify-membership-payment';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+export const metadata: Metadata = {
+    title: 'Photo Gallery',
+    robots: { index: false, follow: false },
+};
+
+type MembershipPayment =
+    | { paid: true; membershipExpiresAt: Date; welcomeEmailSent?: boolean }
+    | { paid: false; membershipExpiresAt?: undefined; welcomeEmailSent?: undefined };
+
+export default async function GalleryPage() {
+    const session = await auth();
+    if (!session?.user) return notFound();
+
+    let exists = false;
+
+    let membershipPayment: MembershipPayment = {
+        paid: false,
+        membershipExpiresAt: undefined,
+    };
+
+    if (session?.user.id) {
+        exists = await checkUserExists(session.user.id);
+        membershipPayment = await verifyMembershipPayment(session.user.id);
+        if (membershipPayment.paid && session.user.email && session.user.firstName)
+            await sendWelcomeEmail(session.user.id, session.user.email, session.user.firstName);
+    }
+
+    return (
+        <main className="flex flex-col items-center gap-8 md:gap-16">
+            <div className="flex justify-center">
+                <Title colour="purple">Photo Gallery</Title>
+            </div>
+            <section className="w-full max-w-[62rem]">
+                <FancyRectangle colour="purple" offset="8" filled fullWidth>
+                    {exists ? (
+                        <div className="flex w-full flex-col gap-4 border-4 border-black bg-white px-4 py-8 text-black md:flex-row md:gap-8 md:p-12">
+                            <h2 className="text-xl">Photos</h2>
+                        </div>
+                    ) : (
+                        <div className="flex w-full flex-col gap-4 border-4 border-black bg-white px-4 py-8 text-black md:flex-row md:gap-8 md:p-12">
+                            <h2 className="text-xl">
+                                Please finish{' '}
+                                <Link href="/join" className="font-bold text-purple">
+                                    signing up
+                                </Link>{' '}
+                                first.
+                            </h2>
+                        </div>
+                    )}
+                </FancyRectangle>
+            </section>
+        </main>
+    );
+}
