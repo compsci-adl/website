fa679f28ab971d777bcb518fc0d3d6ab104b8391
yuan <purehyacinth@outlook.com>
2025-09-10 14:44:33 +0930

    feat: fetch committee members from cms

--- src/data/committee-members.ts
+++ src/data/committee-members.ts
@@ -1,3 +1,6 @@
+import { env } from '@/env.mjs';
+import { fetcher } from '@/lib/fetcher';
+
 type CommitteeMember = {
     name: string;
     position: string;
@@ -6,119 +9,27 @@ type CommitteeMember = {
 
 // List of committee members with name, role and whether they are part of the
 // executive committee
-export const COMMITTEE_MEMBERS: CommitteeMember[] = [
-    {
-        name: 'Yaman Ashqar',
-        position: 'President',
-        exec: true,
-    },
-    {
-        name: 'Leo Li',
-        position: 'Vice-President',
-        exec: true,
-    },
-    {
-        name: 'Christian Dianos',
-        position: 'Treasurer',
-        exec: true,
-    },
-    {
-        name: 'Austin Huppatz',
-        position: 'Secretary',
-        exec: true,
-    },
-    {
-        name: 'David Maslov',
-        position: 'Executive Advisor',
-        exec: true,
-    },
-    {
-        name: 'Zach Anderson',
-        position: 'Partnerships & Sponsorships Manager',
-        exec: true,
-    },
-    {
-        name: 'Lloyd',
-        position: 'Partnerships & Sponsorships Officer',
-    },
-    {
-        name: 'Yuhan Wang',
-        position: 'Design Manager',
-    },
-    {
-        name: 'Prefei Ren',
-        position: 'Design Officer',
-    },
-    {
-        name: 'Lucy Fidock',
-        position: 'Design Officer',
-    },
-    {
-        name: 'Omar Badr',
-        position: 'Social Media & Marketing Manager',
-    },
-    {
-        name: 'Lalisa Thaiprasert',
-        position: 'Social Media & Marketing Officer',
-    },
-    {
-        name: 'Jessica Hu',
-        position: 'Social Media & Marketing Officer',
-    },
-    {
-        name: 'Tim Pham',
-        position: 'Social Media & Marketing Officer',
-    },
-    {
-        name: 'Isaac Sheehan',
-        position: 'Social Media & Marketing Officer',
-    },
-    {
-        name: 'Phoenix Pereira',
-        position: 'Open Source & Infrastructure Manager',
-    },
-    {
-        name: 'Kent Rattley',
-        position: 'Open Source Officer',
-    },
-    {
-        name: 'Asrith Atluri',
-        position: 'Open Source Officer',
-    },
-    {
-        name: 'Willard Gorman',
-        position: 'Events Manager',
-    },
-    {
-        name: 'Mary',
-        position: 'Events Officer',
-    },
-    {
-        name: 'Shreeshail',
-        position: 'Events Officer',
-    },
-    {
-        name: 'Thai Le',
-        position: 'Events Officer',
-    },
-    {
-        name: 'Tristan Coventry',
-        position: 'Events Officer',
-    },
-    {
-        name: 'Jason',
-        position: 'First Year Representative ',
-    },
-    {
-        name: 'Syeda Ramisa',
-        position: 'Postgraduate Representative',
-    },
-    {
-        name: 'Matilda Cotton',
-        position: 'Human Resources Officer',
-    },
-    {
-        name: 'Ben Signorelli',
-        position: 'Duck Lounge Liaison',
-    },
-];
+
+const committeeMemberURL = env.NEXT_PUBLIC_PAYLOAD_URI + '/api/committee-members?limit=50';
+
+export async function fetchCommitteeMember(): Promise<CommitteeMember[]> {
+    try {
+        // Fetching committee member data from payload with fetcher
+        const data = await fetcher.get.query([
+            committeeMemberURL,
+            { cache: 'no-store', prefixUrl: '' },
+        ]);
+
+        // Process the data to match the CommitteeMember interface
+        const committeeMembers: CommitteeMember[] = data.docs.map((committeeMember: any) => ({
+            name: committeeMember.name,
+            position: committeeMember.role,
+            exec: committeeMember.exec,
+        }));
+
+        return committeeMembers;
+    } catch (error) {
+        console.error('Error fetching committeeMember:', error);
+        return [];
+    }
+}
