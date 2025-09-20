ddddccc156b5833931801e6e8f4b2acb294c7461
Asrith <asrithatluri@gmail.com>
2025-09-20 15:06:14 +0930

    fix: change image size and use compressed file format

--- src/data/home.ts
+++ src/data/home.ts
@@ -2,37 +2,37 @@ import type { Image } from '@/components/ImageCarousel';
 
 export const CAROUSEL_IMAGES: Image[] = [
     {
-        src: '/images/home/oweek-2025.jpg',
+        src: '/images/home/oweek-2025.webp',
         alt: 'OWeek 2025',
     },
 
     {
-        src: '/images/home/meet-and-greet-2025-2.jpg',
+        src: '/images/home/meet-and-greet-2025-2.webp',
         alt: 'Meet and Greet 2 2025',
     },
 
     {
-        src: '/images/home/industry-night-2025.jpg',
+        src: '/images/home/industry-night-2025.webp',
         alt: 'Industry Night 2025',
     },
 
     {
-        src: '/images/home/women-in-tech-2025.jpg',
+        src: '/images/home/women-in-tech-2025.webp',
         alt: 'Women in Tech 2025',
     },
 
     {
-        src: '/images/home/two-slices.jpg',
+        src: '/images/home/two-slices.webp',
         alt: 'TWO SLICES',
     },
 
     {
-        src: '/images/home/cocktail-night-2024.jpg',
+        src: '/images/home/cocktail-night-2024.webp',
         alt: 'Cocktail Night 2024',
     },
 
     {
-        src: '/images/home/meet-and-greet-2025-1.jpg',
+        src: '/images/home/meet-and-greet-2025-1.webp',
         alt: 'Meet and Greet 1 2025',
     },
 ];
