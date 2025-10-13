afc887b21ea32cd09246d413cf1afa9e5d072106
phoenixpereira <phoenixpereira99@gmail.com>
2025-10-13 18:06:44 +1030

    feat(settings): Add update linked Google account button

--- src/app/(account)/settings/tabs/AccountSettings.tsx
+++ src/app/(account)/settings/tabs/AccountSettings.tsx
@@ -3,17 +3,31 @@ import Button from '@/components/Button';
 export default function AccountSettings() {
     const keycloakBaseUrl = process.env.NEXT_PUBLIC_LOCAL_KEYCLOAK_URL;
     const keycloakRealm = process.env.NEXT_PUBLIC_AUTH_REALM;
-    const keycloakAccountUrl = `${keycloakBaseUrl}/realms/${keycloakRealm}/account/account-security/signing-in`;
+    const keycloakUpdatePasswordUrl = `${keycloakBaseUrl}/realms/${keycloakRealm}/account/account-security/signing-in`;
+    const keycloakLinkedAccountsUrl = `${keycloakBaseUrl}/realms/${keycloakRealm}/account/account-security/linked-accounts`;
     return (
         <div className="flex flex-col gap-8">
             <div className="flex flex-col gap-4">
-                <h2 className="text-2xl font-bold">Change Password</h2>
+                <h2 className="text-2xl font-bold">Change Account Info</h2>
                 <div className="mb-2 border-b-2 border-black" />
-                <a href={keycloakAccountUrl} target="_blank" rel="noopener noreferrer">
-                    <Button width="w-full" type="button" colour="orange">
-                        Change password in Keycloak
-                    </Button>
-                </a>
+                <p>Update account password</p>
+                <Button
+                    href={keycloakUpdatePasswordUrl}
+                    width="w-full"
+                    type="button"
+                    colour="orange"
+                >
+                    Change password in Keycloak
+                </Button>
+                <p>Update linked Google account</p>
+                <Button
+                    href={keycloakLinkedAccountsUrl}
+                    width="w-full"
+                    type="button"
+                    colour="orange"
+                >
+                    Manage link in Keycloak
+                </Button>
             </div>
         </div>
     );
